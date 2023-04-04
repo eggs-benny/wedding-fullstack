f5861112f585efa7618e3f94b6f82575f89fe324
eggs-benny <benedict.smith87@gmail.com>
2023-04-04 14:45:30 +0100

    working things out with docker, by no means final or ready

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const { sequelize } = require('./models');
-let cors = require('cors')
-
-//routes
-const guests = require('./routes/api/guests')
-const messages = require('./routes/api/messages')
+const { Sequelize } = require('sequelize');
+const cors = require('cors');
 
 const app = express();
 
@@ -13,13 +9,30 @@ app.use(express.json());
 // cors
 app.use(cors({ origin: true, credentials: true }));
 
-// use routes
-app.use('/guests', guests)
-app.use('/messages', messages)
-
-const PORT = 5001; // port number
-app.listen({ port: PORT }, async () => {
-  console.log(`Server up on http:localhost:${PORT}`);
-  await sequelize.authenticate(); // checks db password etc
-  console.log('Database connected');
+// create the database schema automatically
+const sequelize = new Sequelize('wedding_dev', 'benedictsmith', 'root', {
+  host: 'localhost',
+  dialect: 'postgres',
 });
+
+async function createDatabaseSchema() {
+  await sequelize.sync();
+  console.log('Database schema has been created.');
+}
+
+createDatabaseSchema();
+
+
+// routes
+const guests = require('./routes/api/guests');
+const messages = require('./routes/api/messages');
+
+// wrap app.listen in a function that waits for sequelize to sync
+const startServer = async () => {
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(`Server up on http://[IP ADDRESS]:${PORT}`);
+  });
+};
+
+startServer();
